e2dcfdc5ebb8912d0ee59333ff0b37096ae77982
farhan-mazumder <farhanmazumder75@gmail.com>
2024-12-01 21:43:19 +0600

    learned how to initialize and config Typescript.

---
+++ lesson01-setup-first-step/build/js/main.js
@@ -0,0 +1,7 @@
+"use strict";
+const myName = "Farhan Mazumder";
+console.log(myName);
+const a = 3;
+const b = "12";
+const c = "es";
+console.log(b / a);
---
+++ lesson01-setup-first-step/src/main.ts
@@ -0,0 +1,8 @@
+const myName = "Farhan Mazumder";
+console.log(myName);
+
+const a: number = 3;
+const b = "12";
+const c: string = "es";
+
+console.log(b / a);
