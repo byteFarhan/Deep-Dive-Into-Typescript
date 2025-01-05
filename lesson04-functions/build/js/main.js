67add3bb8c41993c989685a16842345f30326b9c
farhan-mazumder <farhanmazumder75@gmail.com>
2025-01-05 23:53:20 +0600

    practice functionbasics in TS.

--- lesson04-functions/build/js/main.js
+++ lesson04-functions/build/js/main.js
@@ -1,2 +1,24 @@
 "use strict";
-console.log("object");
+let users;
+users = 3;
+// Literal Types
+let myName;
+let userName;
+userName = "Farhan";
+// function in TS
+const add = (a, b) => {
+    return a + b;
+};
+const logMsg = (msg) => {
+    console.log(msg);
+};
+const subtract = function (a, b) {
+    return a - b;
+};
+// interface mathFunction { // It will work as same as type mathFunction
+//   (a: number, b: number): number;
+// }
+const multiply = (c, d) => {
+    return c * d;
+};
+logMsg(multiply(3, 4));
--- lesson04-functions/src/main.ts
+++ lesson04-functions/src/main.ts
@@ -1,1 +1,45 @@
-console.log("object");
+// Type Aliases
+type strOrNum = string | number;
+type strOrNumArray = (string | number)[];
+
+type Guitarist = {
+  name: string;
+  active: boolean;
+  albums: strOrNumArray;
+};
+
+type userId = strOrNum;
+
+let users: strOrNum;
+users = 3;
+
+// Literal Types
+let myName: "Farhan";
+let userName: "Farhan" | "Rifat" | "Rakib" | "Shawon";
+userName = "Farhan";
+
+// function in TS
+const add = (a: number, b: number): number => {
+  return a + b;
+};
+
+const logMsg = (msg: string | number): void => {
+  console.log(msg);
+};
+
+const subtract = function (a: number, b: number): number {
+  return a - b;
+};
+
+//EX of Type Ailases for function
+type mathFunction = (a: number, b: number) => number;
+
+// interface mathFunction { // It will work as same as type mathFunction
+//   (a: number, b: number): number;
+// }
+
+const multiply: mathFunction = (c: number, d: number) => {
+  return c * d;
+};
+
+logMsg(multiply(3, 4));
