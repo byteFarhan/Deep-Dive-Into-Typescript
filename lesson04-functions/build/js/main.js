3da0ed5f6a1f141076d9b297e28ebf604f73fbd5
farhan-mazumder <farhanmazumder75@gmail.com>
2025-01-06 21:24:27 +0600

    practice more about function in TS.

--- lesson04-functions/build/js/main.js
+++ lesson04-functions/build/js/main.js
@@ -22,3 +22,50 @@ const multiply = (c, d) => {
     return c * d;
 };
 logMsg(multiply(3, 4));
+// Optional parameter in function //Note: Always put optional parameters end position.
+const sumAll = (a, b, c) => {
+    if (c !== undefined) {
+        // this is called Type gurd.
+        return a + b + c;
+    }
+    return a + b;
+};
+logMsg(sumAll(4, 9));
+logMsg(sumAll(4, 9, 3));
+// Default parameter value in function // Note: Default value not work with function type aliases or function signature
+const addAll = (a = 12, b, c = 10) => {
+    return a + b + c;
+};
+logMsg(addAll(undefined, 3));
+logMsg(addAll(undefined, 3, 2));
+// Rest perameter
+const total = (a, ...nums) => {
+    return a + nums.reduce((prev, curr) => prev + curr);
+};
+logMsg(total(3, 4, 5, 6, 3));
+// return never type in function
+const createError = (msg) => {
+    throw new Error(msg);
+};
+// when function has endless loop in it, then it will return never type.
+const infinite = () => {
+    let i = 1;
+    while (true) {
+        i++;
+        // if (i > 100) break;
+    }
+};
+// Custom type guard
+const isNumber = (value) => {
+    return typeof value === "number" ? true : false;
+};
+// one simple use case of never type in function
+const numOrStr = (value) => {
+    // Type guard
+    if (typeof value === "string")
+        return "string";
+    if (isNumber(value))
+        return "number";
+    //   if (typeof value === "number") return "number";
+    return createError("This should never happen!");
+};
--- lesson04-functions/src/main.ts
+++ lesson04-functions/src/main.ts
@@ -43,3 +43,58 @@ const multiply: mathFunction = (c: number, d: number) => {
 };
 
 logMsg(multiply(3, 4));
+
+// Optional parameter in function //Note: Always put optional parameters end position.
+const sumAll = (a: number, b: number, c?: number): number => {
+  if (c !== undefined) {
+    // this is called Type gurd.
+    return a + b + c;
+  }
+  return a + b;
+};
+
+logMsg(sumAll(4, 9));
+logMsg(sumAll(4, 9, 3));
+
+// Default parameter value in function // Note: Default value not work with function type aliases or function signature
+const addAll = (a: number = 12, b: number, c: number = 10): number => {
+  return a + b + c;
+};
+
+logMsg(addAll(undefined, 3));
+logMsg(addAll(undefined, 3, 2));
+
+// Rest perameter
+const total = (a: number, ...nums: number[]): number => {
+  return a + nums.reduce((prev, curr) => prev + curr);
+};
+
+logMsg(total(3, 4, 5, 6, 3));
+
+// return never type in function
+const createError = (msg: string): never => {
+  throw new Error(msg);
+};
+
+// when function has endless loop in it, then it will return never type.
+const infinite = () => {
+  let i: number = 1;
+  while (true) {
+    i++;
+    // if (i > 100) break;
+  }
+};
+
+// Custom type guard
+const isNumber = (value: any): boolean => {
+  return typeof value === "number" ? true : false;
+};
+
+// one simple use case of never type in function
+const numOrStr = (value: number | string): string => {
+  // Type guard
+  if (typeof value === "string") return "string";
+  if (isNumber(value)) return "number";
+  //   if (typeof value === "number") return "number";
+  return createError("This should never happen!");
+};
