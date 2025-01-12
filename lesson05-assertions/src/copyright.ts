a7ac9c813f4a95d11f8c878cdb736f3dae0408a0
farhan-mazumder <farhanmazumder75@gmail.com>
2025-01-12 22:03:52 +0600

    intialize lesson-06

--- lesson05-assertions/src/copyright.ts
+++ lesson05-assertions/src/copyright.ts
@@ -11,13 +11,13 @@
 // year.textContent = thisYear as string;
 
 // TS soliution no 2.
-// const year = document.getElementById("year") as HTMLSpanElement;
+// const year = <HTMLSpanElement>document.getElementById("year");
 // const thisYear = new Date().getFullYear() as unknown as string;
 // year.setAttribute("datetime", thisYear);
 // year.textContent = thisYear;
 
 // TS soliution no 3.
-const year = <HTMLSpanElement>document.getElementById("year");
+const year = document.getElementById("year") as HTMLSpanElement;
 const thisYear = new Date().getFullYear().toString();
 // console.log(thisYear);
 year.setAttribute("datetime", thisYear);
